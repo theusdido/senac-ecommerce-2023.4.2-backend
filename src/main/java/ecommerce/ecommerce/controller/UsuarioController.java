f6dc666de2d9c7158bba8bc819b1bb17cec6ef17
theusdido <theusdido@hotmail.com>
2023-10-03 21:37:38 -0300

    Salvando Usuário

--- src/main/java/ecommerce/ecommerce/controller/UsuarioController.java
+++ src/main/java/ecommerce/ecommerce/controller/UsuarioController.java
@@ -3,6 +3,8 @@ package ecommerce.ecommerce.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 import ecommerce.ecommerce.model.UsuarioEntity;
 import ecommerce.ecommerce.repository.UsuarioRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class UsuarioController {
     
     @Autowired
     UsuarioRepository usuarioRepositorio;
- 
+    
+    @GetMapping(value="/")
+    public void get(){
+
+    }
+    
     @PostMapping (value = "/usuario")
     public ResponseEntity<UsuarioEntity> salvar(@RequestBody UsuarioEntity usuario) {
         UsuarioEntity _user = usuarioRepositorio.save(usuario);
         return new ResponseEntity<>(_user, HttpStatus.CREATED);
     }
+    
 }
