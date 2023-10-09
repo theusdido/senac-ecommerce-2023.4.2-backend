1fb115346a0e5b43df91ab0128eba4cebaee896d
Edilson Bitencourt <theusdido@hotmail.com>
2023-10-09 18:37:00 -0300

    Exclusão usuário CRUD

--- src/main/java/ecommerce/ecommerce/controller/UsuarioController.java
+++ src/main/java/ecommerce/ecommerce/controller/UsuarioController.java
@@ -1,16 +1,21 @@
 package ecommerce.ecommerce.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ecommerce.ecommerce.model.UsuarioEntity;
 import ecommerce.ecommerce.repository.UsuarioRepository;
+
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -30,4 +35,15 @@ public class UsuarioController {
         return new ResponseEntity<>(_user, HttpStatus.CREATED);
     }
     
+
+    @GetMapping(value="/usuario/listar")
+    public List<UsuarioEntity> getMethodName() {
+        return usuarioRepositorio.findAll();
+    }
+    
+    @DeleteMapping(path ={"/usuario/{id}"})
+    public ResponseEntity <?> delete(@PathVariable long id) {
+        usuarioRepositorio.deleteById(id);
+        return ResponseEntity.ok().build();
+    }    
 }
