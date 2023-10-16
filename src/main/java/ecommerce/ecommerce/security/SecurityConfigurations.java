c48487d24dfc3419e8056817b5f21d5895dbc2a0
theusdido <theusdido@hotmail.com>
2023-10-16 19:25:38 -0300

    SecurityConfiguration

---
+++ src/main/java/ecommerce/ecommerce/security/SecurityConfigurations.java
@@ -0,0 +1,52 @@
+package ecommerce.ecommerce.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations implements WebMvcConfigurer {
+
+    @Autowired
+    SecurityFilter security_filter;
+
+    @Bean
+    public SecurityFilterChain SecurityFilterChain (HttpSecurity http_security) throws Exception {
+        return  http_security                
+                .csrf(csrf -> csrf.disable())                
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers(HttpMethod.POST, "/auth").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/usuario/salvar").permitAll()
+                    .anyRequest().authenticated()
+                    
+                )                                
+                .addFilterBefore(security_filter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth_config)
+    throws Exception {
+        return auth_config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
