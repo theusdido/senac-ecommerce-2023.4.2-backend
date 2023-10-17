35fb7b2b8958bb7d7d1727020d27a7182873a1fb
theusdido <theusdido@hotmail.com>
2023-10-16 21:28:36 -0300

    Authorization Service

--- src/main/java/ecommerce/ecommerce/repository/UsuarioRepository.java
+++ src/main/java/ecommerce/ecommerce/repository/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package ecommerce.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import ecommerce.ecommerce.model.UsuarioEntity;
 
 public interface UsuarioRepository 
 extends JpaRepository<UsuarioEntity, Long> {
-    
+     UserDetails findByEmail(String email);     
 }
